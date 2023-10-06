811f9e6a3e76f98933ca59a8f4838def777591fe
Caleb Wightman <agentcboy@gmail.com>
2023-10-06 10:19:49 -0600

    working devcontainer frontend

--- nutrinova-ui/next.config.js
+++ nutrinova-ui/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  webpack: (config, _) => ({
+    ...config,
+    watchOptions: {
+      ...config.watchOptions,
+      poll: 800,
+      aggregateTimeout: 300,
+    },
+  }),
 }
 
 module.exports = nextConfig
