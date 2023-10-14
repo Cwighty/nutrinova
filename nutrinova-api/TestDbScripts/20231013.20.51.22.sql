e2d54ad10279bcff2d542d995459b3ad40390e9d
Caleb Wightman <agentcboy@gmail.com>
2023-10-14 14:03:36 +0000

    fix public schema not exist, we should consider different schema

--- nutrinova-api/TestDbScripts/20231013.20.51.22.sql
+++ nutrinova-api/TestDbScripts/20231013.20.51.22.sql
@@ -20,7 +20,7 @@ SET row_security = off;
 -- Name: public; Type: SCHEMA; Schema: -; Owner: pg_database_owner
 --
 
-CREATE SCHEMA public;
+CREATE SCHEMA if not exists public;
 
 
 ALTER SCHEMA public OWNER TO pg_database_owner;
