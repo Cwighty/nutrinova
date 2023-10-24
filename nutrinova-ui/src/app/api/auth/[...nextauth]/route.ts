8d232e6e4cb2f6ac68f753af5ef90eba0ffdbe7b
Caleb Wightman <agentcboy@gmail.com>
2023-10-24 23:51:39 +0000

    fix lint again

--- nutrinova-ui/src/app/api/auth/[...nextauth]/route.ts
+++ nutrinova-ui/src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import { options } from "./options";
 
-const handler = NextAuth(options)
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const handler = NextAuth(options);
 
-export { handler as GET, handler as POST}
+export { handler as GET, handler as POST };
