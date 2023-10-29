c62729d4bcee17a7afb3864eb67bf13ecdf9588c
Garrett Vernon <70212190+garv5014@users.noreply.github.com>
2023-10-28 19:50:42 -0600

    Updating endpoints for food search (#27)
    * got one of the controllers to work. removed api key for security
    
    * flattening the call a liitle
    
    * flattened the food and food nutrients
    
    * flattened the food and food nutrients
    
    * removed console log of the token
    
    * fix linting
    
    * got it working locally with .env.local file
    
    * I think they variables have been added in the correct place for prod.
    
    * Fixed spacing in Controller?
    
    * added to action
    
    ---------
    
    Co-authored-by: Caleb Wightman <agentcboy@gmail.com>

---
+++ nutrinova-api/NutrinovaData/ResponseModels/FoodSearchResponseModel.cs
@@ -0,0 +1,14 @@
+namespace NutrinovaData.ResponseModels;
+
+public class FoodSearchResponseModel
+{
+    public int totalHits { get; set; }
+
+    public int currentPage { get; set; }
+
+    public int totalPages { get; set; }
+
+    public FoodSearchCriteria foodSearchCriteria { get; set; }
+
+    public List<Food> foods { get; set; }
+}
