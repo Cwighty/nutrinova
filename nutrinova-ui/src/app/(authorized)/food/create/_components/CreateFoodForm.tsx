f717f3a5a16cd08b4c344b5bcdd3676efe4d42f7
Caleb Wightman <agentcboy@gmail.com>
2023-11-04 00:46:33 +0000

    lay out create food page

--- nutrinova-ui/src/app/(authorized)/food/create/_components/CreateFoodForm.tsx
+++ nutrinova-ui/src/app/(authorized)/food/create/_components/CreateFoodForm.tsx
@@ -1,28 +1,162 @@
 'use client'
-import createAuthenticatedAxiosInstanceFactory from '@/services/axiosRequestFactory';
-import React from 'react';
+import React, { useState } from 'react';
+import {
+    Button,
+    TextField,
+    IconButton,
+    List,
+    ListItem,
+    ListItemSecondaryAction,
+    ListItemText,
+    Grid,
+    Paper,
+    Alert,
+    Box,
+} from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { CreateFoodNutrientRequestModel } from '../_models/createFoodNutrientRequestModel';
 import { CreateFoodRequestModel } from '../_models/createFoodRequest';
+import { AddNutrientDialog } from './AddNutrientDialog';
 
-interface Props {
-    // Define any props here
-}
-
-const CreateFoodForm: React.FC<Props> = () => {
-
-    const handleSubmit = async () => {
-        const apiClient = await createAuthenticatedAxiosInstanceFactory({ additionalHeaders: {}, origin: "client" });
-        const newFood: CreateFoodRequestModel = {
-            description: "custom food",
-            servingSize: 1,
-            foodNutrients: [],
-        }
-        await apiClient.post('/food', newFood);
-    };
-    return (
-        <>
-            <button onClick={handleSubmit}>Test</button>
-        </>
-    );
+const initialNutrient: CreateFoodNutrientRequestModel = {
+    nutrientId: '',
+    amount: 0,
+    unitId: ''
 };
 
-export default CreateFoodForm;
+const availableNutrients = [
+    { id: '1', name: 'Protein' },
+    { id: '2', name: 'Fat' },
+    // ... other nutrients
+];
+
+const availableUnits = [
+    { id: 'g', name: 'grams' },
+    { id: 'mg', name: 'milligrams' },
+    // ... other units
+];
+
+export default function CreateFoodForm() {
+    const [foodFormState, setFoodFormState] = useState<CreateFoodRequestModel>({
+        description: '',
+        servingSize: undefined,
+        unit: '',
+        note: '',
+        foodNutrients: [initialNutrient]
+    });
+
+    const [newNutrient, setNewNutrient] = useState<CreateFoodNutrientRequestModel>({ ...initialNutrient });
+
+    const handleAddNutrient = () => {
+        setFoodFormState({
+            ...foodFormState,
+            foodNutrients: [...foodFormState.foodNutrients, newNutrient]
+        });
+        setNewNutrient({ ...initialNutrient });
+    };
+
+    const handleRemoveNutrient = (index: number) => {
+        const updatedNutrients = [...foodFormState.foodNutrients];
+        updatedNutrients.splice(index, 1);
+        setFoodFormState({ ...foodFormState, foodNutrients: updatedNutrients });
+    };
+
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        // Form submission logic goes here
+        console.log(foodFormState);
+    };
+
+    return (
+        <Paper elevation={1} sx={{ padding: 2 }}>
+            <form onSubmit={handleSubmit}>
+                <Grid container spacing={2}>
+                    {/* Food Name */}
+                    <Grid item xs={12} md={6}>
+                        <TextField
+                            label="Food Name"
+                            value={foodFormState.description}
+                            onChange={(e) => setFoodFormState(
+                                { ...foodFormState, description: e.target.value }
+                            )}
+                            fullWidth
+                            margin="normal"
+                        />
+                    </Grid>
+
+                    {/* Serving Size */}
+                    <Grid item xs={12} md={3}>
+                        <TextField
+                            label="Serving Size"
+                            type="number"
+                            value={foodFormState.servingSize}
+                            onChange={(e) => setFoodFormState(
+                                { ...foodFormState, servingSize: Number(e.target.value) }
+                            )}
+                            fullWidth
+                            margin="normal"
+                        />
+                    </Grid>
+
+                    <Grid item xs={12} md={3}>
+                        <TextField
+                            label="Unit"
+                            value={foodFormState.unit}
+                            onChange={(e) => setFoodFormState(
+                                { ...foodFormState, unit: e.target.value }
+                            )}
+                            fullWidth
+                            margin="normal"
+                        />
+                    </Grid>
+
+                    {/* Note */}
+                    <Grid item xs={12}>
+                        <TextField
+                            label="Note"
+                            multiline
+                            rows={4}
+                            value={foodFormState.note}
+                            onChange={(e) => setFoodFormState(
+                                { ...foodFormState, note: e.target.value }
+                            )}
+                            fullWidth
+                            margin="normal"
+                        />
+                    </Grid>
+                </Grid>
+
+                <List>
+                    <AddNutrientDialog
+                        newNutrient={newNutrient}
+                        setNewNutrient={setNewNutrient}
+                        handleAddNutrient={handleAddNutrient}
+                        availableNutrients={availableNutrients}
+                        availableUnits={availableUnits}
+                    />
+                    {foodFormState.foodNutrients.length === 0 &&
+                        <Alert severity='warning'>Please add at least one nutrient</Alert>
+                    }
+                    {foodFormState.foodNutrients.map((nutrient, index) => (
+                        <ListItem key={index} divider>
+                            <ListItemText
+                                primary={availableNutrients.find((n) => n.id === nutrient.nutrientId)?.name}
+                                secondary={`${nutrient.amount} ${availableUnits.find((u) => u.id === nutrient.unitId)?.name}`}
+                            />
+                            <ListItemSecondaryAction>
+                                <IconButton edge="end" aria-label="delete" onClick={() => handleRemoveNutrient(index)}>
+                                    <DeleteIcon />
+                                </IconButton>
+                            </ListItemSecondaryAction>
+                        </ListItem>
+                    ))}
+                </List>
+                <Box display="flex" justifyContent="flex-end">
+                    <Button type="submit" variant="contained" color="primary">
+                        Create
+                    </Button>
+                </Box>
+            </form>
+        </Paper>
+    );
+}
