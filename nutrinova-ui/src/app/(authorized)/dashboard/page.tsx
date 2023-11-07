5e4a9dba802e8d9073e3b66ddfca3246364928e4
Caleb Wightman <agentcboy@gmail.com>
2023-11-07 22:45:15 +0000

    merge

--- nutrinova-ui/src/app/(authorized)/dashboard/page.tsx
+++ nutrinova-ui/src/app/(authorized)/dashboard/page.tsx
@@ -2,18 +2,10 @@ import { PageContainer } from "@/components/PageContainer";
 import { Typography } from "@mui/material";
 import { AtomSpinner } from "@/components/AtomSpinner/AtomSpinner";
 
-
 export const metadata = {
   title: "Dashboard",
 };
 
-export default function DashboardPage() {
-    return (
-        <PageContainer title={metadata.title}>
-            <Typography variant={"h3"}>Dashboard</Typography>
-            <AtomSpinner />
-        </PageContainer>
-    );
 export default function DashboardPage() {
   return (
     <PageContainer title={metadata.title}>
