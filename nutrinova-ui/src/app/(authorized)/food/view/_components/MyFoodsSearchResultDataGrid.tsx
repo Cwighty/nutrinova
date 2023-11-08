6d9dc85eb01ed8c15e579905d97b4fdaf60c04ba
garv5014 <garv5014@gmail.com>
2023-11-07 18:48:22 -0700

    reinstalled and fixed lint unused vars

--- nutrinova-ui/src/app/(authorized)/food/view/_components/MyFoodsSearchResultDataGrid.tsx
+++ nutrinova-ui/src/app/(authorized)/food/view/_components/MyFoodsSearchResultDataGrid.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { FoodSearchResult } from "@/app/(authorized)/food/_models/foodSearchResult";
-import { DataGrid, GridColDef, GridRowParams } from "@mui/x-data-grid";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
 interface MyFoodsSearchResultDataGridProps {
   rows: FoodSearchResult[];
@@ -14,7 +14,7 @@ export const MyFoodsSearchResultDataGrid = ({
     { field: "fdcId", headerName: "ID", width: 100 },
     { field: "description", headerName: "Description", width: 500 },
   ];
-  const handleRowClick = (row: GridRowParams<FoodSearchResult>) => {
+  const handleRowClick = () => {
     // router.push(`/food/search/detail?food=` + JSON.stringify(row.row));
   };
   return (
