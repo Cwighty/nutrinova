6b3610786b882a875a87a579265e1684bc40cb55
Caleb Wightman <agentcboy@gmail.com>
2023-11-16 03:07:24 +0000

    passing tests

--- nutrinova-api/NutrinovaData.UnitTests/FoodTotalerTests.cs
+++ nutrinova-api/NutrinovaData.UnitTests/FoodTotalerTests.cs
@@ -48,4 +48,43 @@ public class FoodTotalerTests
         Assert.That(result[0].Amount, Is.EqualTo(100));
         Assert.That(result[0].Unit?.Description, Is.EqualTo("mg"));
     }
+
+    [Test]
+    public void GetNutrientSummariesWithMultipleFoodsSameNutrientReturnsCorrectSum()
+    {
+        // Arrange
+        var foodPlans = new List<FoodPlan>
+    {
+        new()
+        {
+            FoodPlanNutrients = new List<FoodPlanNutrient>
+            {
+                new()
+                {
+                    Nutrient = new Nutrient { NutrientName = "Vitamin C", Id = 1 },
+                    Amount = 50,
+                    Unit = new Unit { Id = 1, Description = "mg" },
+                    NutrientId = 1,
+                },
+                new()
+                {
+                    Nutrient = new Nutrient { NutrientName = "Vitamin C", Id = 1 },
+                    Amount = 75,
+                    Unit = new Unit { Id = 1, Description = "mg" },
+                    NutrientId = 1,
+                },
+            },
+        },
+    };
+
+        // Act
+        var result = FoodTotaler.GetNutrientSummaries(foodPlans);
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.That(result.Count, Is.EqualTo(1));
+        Assert.That(result[0].Name, Is.EqualTo("Vitamin C"));
+        Assert.That(result[0].Amount, Is.EqualTo(125)); // Sum of 50 and 75
+        Assert.That(result[0].Unit?.Description, Is.EqualTo("mg"));
+    }
 }
