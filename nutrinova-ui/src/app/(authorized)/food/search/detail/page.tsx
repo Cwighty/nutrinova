87a14669ae603ecd0706d2463702f7093205788c
andrewbengordon <andrewbengordon@gmail.com>
2023-11-16 10:27:53 -0700

    refactored pages, more mobile friendly

--- nutrinova-ui/src/app/(authorized)/food/search/detail/page.tsx
+++ nutrinova-ui/src/app/(authorized)/food/search/detail/page.tsx
@@ -1,24 +1,10 @@
 "use client";
-import {
-  Card,
-  CardContent,
-  List,
-  ListItem,
-  Typography,
-  Chip,
-  Button,
-  Box,
-} from "@mui/material";
 import React from "react";
 import CenteredSpinnerWithBackdrop from "@/components/CenteredSpinnerOverlay";
-import {
-  ImportFoodResponse,
-  useGetFoodSearchResultQuery,
-  useImportFoodMutation,
-} from "../../foodHooks";
-import { useRouter } from "next/navigation";
+import { useImportFoodMutation } from "../../foodHooks";
 import { FoodSearchFilterParams } from "../../_models/foodSearchFilterParams";
-import { FoodSearchDetailSkeleton } from "@/components/skeletons/FoodSearchDetailSkeleton";
+import { PageContainer } from "@/components/PageContainer";
+import { SearchFoodsDetailCard } from "@/app/(authorized)/food/search/_components/SearchFoodsDetailCard";
 
 interface PageProps {
   searchParams: { [key: string]: string | string[] | undefined };
@@ -30,96 +16,18 @@ export default function Page({ searchParams }: PageProps) {
     filterOption: searchParams["filterOption"] as string,
   };
   const fdcIdParam = searchParams["fdcid"];
-  const fdcId = parseInt(fdcIdParam as string);
-  const { data: food, isLoading: foodIsLoading } = useGetFoodSearchResultQuery(
-    searchFilterParams,
-    fdcId,
-  );
 
   const importFoodMutation = useImportFoodMutation();
-  const router = useRouter();
-
-  const handleImport = () => {
-    importFoodMutation.mutate(food!.fdcId, {
-      onSuccess: (response: ImportFoodResponse) => {
-        router.push("/food/" + response.id + "/edit");
-      },
-    });
-  };
-
-  if (!food || foodIsLoading) {
-    return <FoodSearchDetailSkeleton />;
-  }
 
   return (
-    <>
+    <PageContainer title={"Search Details"}>
       {importFoodMutation.isPending && (
         <CenteredSpinnerWithBackdrop message="Importing food..." />
       )}
-      <Card sx={{ maxWidth: 600, my: 1, mx: "auto", p: 1.25 }}>
-        <CardContent>
-          <Box display="flex" justifyContent="space-between" alignItems="start">
-            <Box>
-              <Typography variant="h5" gutterBottom>
-                {food.description}
-              </Typography>
-              {food.brandName && (
-                <Typography variant="h6" color="primary" gutterBottom>
-                  {food.brandName}
-                </Typography>
-              )}
-            </Box>
-            <Button variant="contained" color="primary" onClick={handleImport}>
-              Import
-            </Button>
-          </Box>
-          {food.ingredients && (
-            <Box sx={{ mt: 1.25 }}>
-              {food.ingredients.split(",").map((ingredient, index) => (
-                <Chip
-                  label={ingredient.trim()}
-                  variant="outlined"
-                  size="small"
-                  sx={{ m: 0.5 }}
-                  key={index}
-                />
-              ))}
-            </Box>
-          )}
-          {food.servingSizeWithUnits && (
-            <Typography variant="body1" paragraph sx={{ my: 1.5 }}>
-              Serving Size: {parseFloat(food.servingSizeWithUnits).toFixed(2)}{" "}
-              GRM
-            </Typography>
-          )}
-          {food.foodNutrients && food.foodNutrients.length > 0 && (
-            <>
-              <Typography variant="h6" gutterBottom sx={{ mt: 1 }}>
-                Nutrients
-              </Typography>
-              <List>
-                {food.foodNutrients.map((nutrient) => (
-                  <ListItem
-                    key={nutrient.nameWithAmountAndUnit}
-                    sx={{ py: 0.5 }}
-                    divider
-                  >
-                    <Typography variant="body2">
-                      {nutrient.nutrientName}
-                    </Typography>
-                    <Typography
-                      variant="body2"
-                      sx={{ ml: "auto", fontWeight: "bold" }}
-                    >
-                      {nutrient.value} {nutrient.unitName}
-                    </Typography>
-                  </ListItem>
-                ))}
-              </List>
-            </>
-          )}
-        </CardContent>
-      </Card>
-    </>
+      <SearchFoodsDetailCard
+        searchFilterParams={searchFilterParams}
+        fdcIdParam={fdcIdParam}
+      />
+    </PageContainer>
   );
 }
--- nutrinova-ui/src/app/(authorized)/food/view/details/page.tsx
+++ nutrinova-ui/src/app/(authorized)/food/view/details/page.tsx
@@ -1,18 +1,7 @@
 "use client";
-import {
-  Card,
-  CardContent,
-  Divider,
-  List,
-  ListItem,
-  Typography,
-  Chip,
-  CardActions,
-  Button,
-  Box,
-} from "@mui/material";
 import React from "react";
-import { useGetFoodByIdQuery } from "@/app/(authorized)/food/foodHooks";
+import { PageContainer } from "@/components/PageContainer";
+import { MyFoodsDetailCard } from "@/app/(authorized)/food/view/_components/MyFoodsDetailCard";
 
 interface PageProps {
   searchParams: { [key: string]: string | string[] | undefined };
@@ -21,86 +10,9 @@ interface PageProps {
 export default function Page({ searchParams }: PageProps) {
   const foodId = searchParams["foodId"] as string;
 
-  const { data } = useGetFoodByIdQuery(foodId);
-
   return (
-    <>
-      <Card sx={{ maxWidth: 600, my: 1, mx: "auto", p: 1.25 }}>
-        <CardContent>
-          <Box display="flex" justifyContent="space-between" alignItems="start">
-            <Box>
-              <Typography variant="h5" gutterBottom>
-                {data?.description}
-              </Typography>
-              {data?.brandName && (
-                <Typography variant="h6" color="primary" gutterBottom>
-                  {data?.brandName}
-                </Typography>
-              )}
-            </Box>
-            <Button variant="contained" color="primary">
-              Edit
-            </Button>
-          </Box>
-          {data?.ingredients && (
-            <Box sx={{ mt: 1.25 }}>
-              {data?.ingredients
-                .split(",")
-                .map((ingredient, index) => (
-                  <Chip
-                    label={ingredient.trim()}
-                    variant="outlined"
-                    size="small"
-                    sx={{ m: 0.5 }}
-                    key={index}
-                  />
-                ))}
-            </Box>
-          )}
-          {data?.servingSizeWithUnits && (
-            <Typography variant="body1" paragraph sx={{ my: 1.5 }}>
-              Serving Size: {data?.servingSize.toFixed(2)}{" "}
-              {data?.servingSizeUnit}
-            </Typography>
-          )}
-          {data?.foodNutrients && data?.foodNutrients.length > 0 && (
-            <>
-              <Typography variant="h6" gutterBottom sx={{ mt: 1 }}>
-                Nutrients
-              </Typography>
-              <Divider />
-              <List dense>
-                {data?.foodNutrients.map((nutrient) => (
-                  <ListItem
-                    key={nutrient.nameWithAmountAndUnit}
-                    sx={{ py: 0.5 }}
-                    divider
-                  >
-                    <Typography variant="body2">
-                      {nutrient.nutrientName}
-                    </Typography>
-                    <Typography
-                      variant="body2"
-                      sx={{ ml: "auto", fontWeight: "bold" }}
-                    >
-                      {nutrient.value} {nutrient.unitName}
-                    </Typography>
-                  </ListItem>
-                ))}
-              </List>
-            </>
-          )}
-          <Typography variant="h6" gutterBottom sx={{ mt: 1 }}>
-            Notes
-          </Typography>
-          <Divider />
-          {data?.note && (
-            <Typography variant="body2" sx={{ mt: 1 }}>
-              {data?.note}
-            </Typography>
-          )}
-        </CardContent>
-      </Card>
-    </>
+    <PageContainer title={"Food Details"}>
+      <MyFoodsDetailCard foodId={foodId} />
+    </PageContainer>
   );
 }
