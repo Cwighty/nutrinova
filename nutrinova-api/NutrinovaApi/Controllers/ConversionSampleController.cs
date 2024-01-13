db689abfd0f751eb513b37b6551220e803b9dc9e
Caleb Wightman <agentcboy@gmail.com>
2024-01-12 19:50:12 -0700

    bleh 2

--- nutrinova-api/NutrinovaApi/Controllers/ConversionSampleController.cs
+++ nutrinova-api/NutrinovaApi/Controllers/ConversionSampleController.cs
@@ -20,10 +20,18 @@ public class ConversionSampleController : ControllerBase
   [HttpPost]
   public async Task<IActionResult> CreateNewSample(CreateFoodConversionSampleRequestModel foodConversionSample)
   {
+    var food = await context.FoodPlans.FindAsync(foodConversionSample.FoodPlanId);
+
+    if (food == null)
+    {
+      return BadRequest("Food plan not found");
+    }
+
     var sample = new FoodConversionSample
     {
       Id = Guid.NewGuid(),
-      FoodPlanId = foodConversionSample.FoodPlanId,
+      FoodPlanId = food.Id,
+      FoodPlan = food,
       FoodServingsPerMeasurement = foodConversionSample.FoodServingsPerMeasurement,
       MeasurementUnitId = foodConversionSample.MeasurementUnitId,
       CreatedAt = DateTime.UtcNow,
