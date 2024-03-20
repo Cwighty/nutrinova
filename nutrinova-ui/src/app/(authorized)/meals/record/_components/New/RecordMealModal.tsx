691ad2d2fa4bc75b9f88d92529a39f64ffdfdf18
Caleb <agentcboy@gmail.com>
2024-03-20 22:41:06 +0000

    remove dialog title

--- nutrinova-ui/src/app/(authorized)/meals/record/_components/New/RecordMealModal.tsx
+++ nutrinova-ui/src/app/(authorized)/meals/record/_components/New/RecordMealModal.tsx
@@ -1,5 +1,4 @@
-import { CloseSharp } from "@mui/icons-material";
-import { Box, Dialog, DialogContent, DialogTitle, IconButton } from "@mui/material";
+import { Dialog, DialogContent } from "@mui/material";
 import { RecordMeal } from "./RecordMeal";
 
 interface RecordMealModalProps {
@@ -11,14 +10,6 @@ const RecordMealModal: React.FC<RecordMealModalProps> = ({ open, handleClose = u
   return (
     <>
       <Dialog open={open} onClose={handleClose} fullWidth>
-        <DialogTitle>
-          <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-            Record Meal
-            <IconButton onClick={handleClose} sx={{ ml: 'auto' }}>
-              <CloseSharp />
-            </IconButton>
-          </Box>
-        </DialogTitle>
         <DialogContent sx={{ minHeight: 650 }}>
           <RecordMeal />
         </DialogContent>
