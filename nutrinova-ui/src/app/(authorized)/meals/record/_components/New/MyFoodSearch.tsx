2a4af236dc16c3d36571693b2875e729f9862a94
Caleb <agentcboy@gmail.com>
2024-03-20 20:54:53 +0000

    create button and filter menu

--- nutrinova-ui/src/app/(authorized)/meals/record/_components/New/MyFoodSearch.tsx
+++ nutrinova-ui/src/app/(authorized)/meals/record/_components/New/MyFoodSearch.tsx
@@ -1,12 +1,62 @@
+"use client"
+import { Add } from "@mui/icons-material";
+import { Box, Button, Menu, MenuItem, Typography } from "@mui/material";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 interface MyFoodSearchProps {
   searchKeyword: string;
 }
 
 export const MyFoodSearch: React.FC<MyFoodSearchProps> = ({ searchKeyword }: MyFoodSearchProps) => {
+
+  const router = useRouter();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [sortby, setSortby] = useState<string>("Date Created");
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  }
+  const handleClose = (sortBy: string) => {
+    setSortby(sortBy);
+    setAnchorEl(null);
+  }
+
+
+
   return (
-    <div>
-      <h1>Search All</h1>
-      <p>Search keyword: {searchKeyword}</p>
-    </div>
+    <Box sx={{ p: 2 }}>
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <Button variant="text" color="primary" onClick={() => router.push("/food/create")}>
+          <Add /> Create Food
+        </Button>
+        {searchKeyword}
+      </Box>
+      <Box sx={{ display: "flex", justifyContent: "space-between", mt: 2 }}>
+        <Typography variant="h5">My Food</Typography>
+        <div>
+          <Button
+            id="basic-button"
+            aria-controls={open ? 'basic-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            onClick={handleClick}
+          >
+            <Typography variant="subtitle1">{sortby}</Typography>
+          </Button>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'basic-button',
+            }}
+          >
+            <MenuItem onClick={() => handleClose("Date Created")}>Date Created</MenuItem>
+          </Menu>
+        </div>
+      </Box>
+    </Box>
   )
 };
--- nutrinova-ui/src/app/(authorized)/meals/record/_components/New/MyRecipeSearch.tsx
+++ nutrinova-ui/src/app/(authorized)/meals/record/_components/New/MyRecipeSearch.tsx
@@ -1,13 +1,59 @@
+import { Add } from "@mui/icons-material";
+import { Box, Button, Menu, MenuItem, Typography } from "@mui/material";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 interface MyRecipeSearchProps {
   searchKeyword: string;
 }
 
 export const MyRecipeSearch: React.FC<MyRecipeSearchProps> = ({ searchKeyword }: MyRecipeSearchProps) => {
+
+  const router = useRouter();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [sortby, setSortby] = useState<string>("Date Created");
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  }
+  const handleClose = (sortBy: string) => {
+    setSortby(sortBy);
+    setAnchorEl(null);
+  }
+
   return (
-    <div>
-      <h1>Search All</h1>
-      <p>Search keyword: {searchKeyword}</p>
-    </div>
+    <Box sx={{ p: 2 }}>
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <Button variant="text" color="primary" onClick={() => router.push("/recipes/create")}>
+          <Add /> Create Recipe
+        </Button>
+        {searchKeyword}
+      </Box>
+      <Box sx={{ display: "flex", justifyContent: "space-between", mt: 2 }}>
+        <Typography variant="h5">My Recipes</Typography>
+        <div>
+          <Button
+            id="basic-button"
+            aria-controls={open ? 'basic-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            onClick={handleClick}
+          >
+            <Typography variant="subtitle1">{sortby}</Typography>
+          </Button>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'basic-button',
+            }}
+          >
+            <MenuItem onClick={() => handleClose("Date Created")}>Date Created</MenuItem>
+          </Menu>
+        </div>
+      </Box>
+    </Box>
   )
 };
