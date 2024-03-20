2f9c5401c253e12190eacbbd392f495ac5495a90
Caleb <agentcboy@gmail.com>
2024-03-20 21:25:34 +0000

    all page section with filter

--- nutrinova-ui/src/app/(authorized)/meals/record/_components/New/SearchAll.tsx
+++ nutrinova-ui/src/app/(authorized)/meals/record/_components/New/SearchAll.tsx
@@ -1,6 +1,8 @@
 import { useGetFoodSearchResultsQuery } from "@/app/(authorized)/food/foodHooks";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { PrepMealCard } from "./PrepMealCard";
+import { FilterMenu } from "./FilterMenu";
+import { useState } from "react";
 
 interface SearchAllProps {
   searchKeyword: string;
@@ -13,14 +15,24 @@ export const SearchAll: React.FC<SearchAllProps> = ({ searchKeyword }: SearchAll
   };
 
   const { data, isLoading } = useGetFoodSearchResultsQuery(filterParams);
+  const options = ["Most Recent", "Name", "Calories"];
+  const [sortBy, setSortBy] = useState<string>(options[0]);
 
   return (
-    <Box>
+    <Box sx={{ p: 2 }}>
+      <Box sx={{ display: "flex", justifyContent: "space-between", mt: 2 }}>
+        <Typography variant="h5">History</Typography>
+        <FilterMenu options={options} selectedFilter={sortBy} onFilterChange={(filter) => setSortBy(filter)} />
+      </Box>
       {isLoading && <div>Loading...</div>}
       {data && <div>{data.length} results</div>}
-      {data && data.slice(0, 10).map((x) => {
+      {data && data.slice(0, 5).map((x) => {
         return <PrepMealCard key={x.id} description={x.description} amount={0} unit={x.servingSizeUnit} calories={0} />
       })}
+      <Box sx={{ display: "flex", justifyContent: "space-between", mt: 2 }}>
+        <Typography variant="h5">All</Typography>
+        <FilterMenu options={options} selectedFilter={sortBy} onFilterChange={(filter) => setSortBy(filter)} />
+      </Box>
     </Box>
   )
 };
